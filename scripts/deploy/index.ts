4d1362aed53b42a2df769b1d48fbfe9a0aa426ea
marco <marco3jp+git@gmail.com>
2021-02-12 04:26:24 +0900

    mdを変換してhtmlとして吐き出せる環境の整備、実装

---
+++ scripts/deploy/index.ts
@@ -0,0 +1,90 @@
+const showdown = require('showdown');
+const mkdirp = require('mkdirp')
+import {config} from "../config";
+import * as fs from "fs";
+
+
+const splitDir = __dirname.split("/");
+
+// TODO: プロジェクトルートの実装が他に思い浮かばないので誰か頼んだ
+splitDir.splice(splitDir.length - 2, 2);
+
+let projectRoot = "";
+
+splitDir.forEach(dirname => {
+    projectRoot += dirname + "/";
+})
+
+const outDir = projectRoot + config.out_dir;
+
+loadFile(config.markdown_dir);
+
+copyFiles();
+
+function loadFile(relativePath: string) {
+    const files = fs.readdirSync(projectRoot + relativePath);
+    files.forEach(file => {
+        if (file.includes(".md")) {
+            exportHTML(relativePath + "/", file);
+        } else if (file.includes(".html")) {
+            exportHTML(relativePath + "/", file, true);
+        } else {
+            loadFile(relativePath + "/" + file);
+        }
+    })
+}
+
+function exportHTML(relativeDirPath: string, fileName: string, disableConvert: boolean = false) {
+    const file = fs.readFileSync(projectRoot + relativeDirPath + fileName, {encoding: "utf8"});
+    let exportDirPath = ""
+    let paths = relativeDirPath.split("/");
+    paths.splice(0, 1);
+    paths.forEach(path => {
+        exportDirPath += "/" + path;
+    });
+
+
+    let sourceCode = "";
+
+    if (disableConvert) {
+        sourceCode = file;
+    } else {
+        const converter = new showdown.Converter();
+        sourceCode = converter.makeHtml(file);
+        fileName = fileName.replace(".md", ".html");
+    }
+
+    sourceCode = mergeTemplate(sourceCode, exportDirPath.split("/").length - 2);
+
+
+    mkdirp(outDir + exportDirPath).then(() => {
+        fs.writeFileSync(outDir + exportDirPath + fileName, sourceCode);
+    })
+}
+
+function mergeTemplate(convertedString: string, depth: number): string {
+    let template = fs.readFileSync(projectRoot + config.template.dir + "/" + config.template.html, {encoding: "utf8"});
+    template = preReplace(template);
+
+    template = template.replace(config.template.replace_token.converted_markdown, convertedString);
+    let relativePathToken = "";
+    for (let i = 0; i < depth; i++) {
+        relativePathToken += "../";
+    }
+    const relativePathRegex = new RegExp(config.template.replace_token.relative_path, 'g');
+    return template.replace(relativePathRegex, relativePathToken);
+}
+
+function preReplace(template) {
+    template = template.replace(config.template.replace_token.css, config.template.css)
+    template = template.replace(config.template.replace_token.js.theme, config.template.js.theme);
+    return template;
+}
+
+function copyFiles() {
+    let css = fs.readFileSync(projectRoot + config.template.dir + "/" + config.template.css, {encoding: "utf8"});
+    let theme = fs.readFileSync(projectRoot + config.template.dir + "/" + config.template.js.theme, {encoding: "utf8"});
+
+    fs.writeFileSync(projectRoot + config.out_dir + "/" + config.template.css, css);
+    fs.writeFileSync(projectRoot + config.out_dir + "/" + config.template.js.theme, theme);
+}
---
+++ scripts/deploy/model/config.ts
@@ -0,0 +1,41 @@
+export default interface DeployConfig {
+    markdown_dir: string,
+    out_dir: string
+    template: {
+        dir: string,
+        html: string,
+        css: string,
+        js: {
+            theme: string,
+        },
+        replace_token: {
+            css: string,
+            js: {
+                theme: string,
+            }
+            converted_markdown: string
+            relative_path: string
+        }
+    }
+}
+
+export const defaultDeployConfig: DeployConfig = {
+    markdown_dir: "md",
+    out_dir: "dist",
+    template: {
+        dir: 'template',
+        html: 'template.html',
+        css: 'style.css',
+        js: {
+            theme: 'theme.js',
+        },
+        replace_token: {
+            css: "{{ template_css }}",
+            js: {
+                theme: "{{ template_js_theme }}"
+            },
+            converted_markdown: "{{ markdown }}",
+            relative_path: "{{ relative_path }}",
+        }
+    }
+}
