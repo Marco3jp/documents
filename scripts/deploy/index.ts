14f3a167c42c86c3333b873c3cc9eb0e3b539b4d
marco <marco3jp+git@gmail.com>
2021-02-27 07:54:39 +0900

    deploy時の呼び出しを調整

--- scripts/deploy/index.ts
+++ scripts/deploy/index.ts
@@ -1,8 +1,6 @@
-import * as fs from "fs";
 import {Converter} from "./converter";
 import {config} from "../config";
-
-const mkdirp = require('mkdirp')
+import Duplicator from "./duplicater";
 
 const splitDir = __dirname.split("/");
 
@@ -18,17 +16,9 @@ splitDir.forEach(dirname => {
 const OUT_DIR = PROJECT_ROOT + config.out_dir;
 
 const converter = new Converter(PROJECT_ROOT, OUT_DIR);
+const duplicator = new Duplicator(PROJECT_ROOT, OUT_DIR);
 
 converter.processFiles(config.markdown_dir);
 
-copyFiles();
-
-function copyFiles() {
-    let css = fs.readFileSync(PROJECT_ROOT + config.template.dir + "/" + config.template.css, {encoding: "utf8"});
-    let theme = fs.readFileSync(PROJECT_ROOT + config.template.dir + "/" + config.template.js.theme, {encoding: "utf8"});
-
-    mkdirp(OUT_DIR).then(() => {
-        fs.writeFileSync(OUT_DIR + "/" + config.template.css, css);
-        fs.writeFileSync(OUT_DIR + "/" + config.template.js.theme, theme);
-    });
-}
+duplicator.duplicateTemplate();
+duplicator.duplicateManuscript(config.markdown_dir);
