f94d4e18628e182a70afea84a1d2a175e9581203
marco <marco3jp+git@gmail.com>
2021-02-27 07:55:08 +0900

    原稿ファイルの吐き出し場所を設定に追加

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,8 +1,17 @@
 import DeployConfig, {defaultDeployConfig} from "./deploy/model/config";
 import RuntimeConfig, {defaultRuntimeConfig} from "./runtime/model/config";
 
-export const config: DeployConfig & RuntimeConfig = {
+export interface CommonConfig {
+    manuscript_dir: string
+}
+
+export const defaultCommonConfig: CommonConfig = {
+    manuscript_dir: "manuscript"
+}
+
+export const config: DeployConfig & RuntimeConfig & CommonConfig = {
     ...defaultDeployConfig,
     ...defaultRuntimeConfig,
+    ...defaultCommonConfig,
     is_hidden_draft: false,
 }
