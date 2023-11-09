650a06ccca39aac812e671171d31bf928d5b6559
marco <marco3jp+git@gmail.com>
2023-11-09 23:21:17 +0900

    新しいプロジェクトで記事を読み込めるように整備

--- app/src/app/[...paths]/page.tsx
+++ app/src/app/[...paths]/page.tsx
@@ -1,5 +1,13 @@
 import {config} from "../../../model/config";
 import {getFilePathLists} from "@/lib/fs";
+import * as fs from "fs";
+import {markedConfig} from "../../../marked.config";
+import {marked} from "marked";
+import {mangle} from "marked-mangle";
+import {gfmHeadingId} from "marked-gfm-heading-id";
+import hljs from 'highlight.js';
+import {markedHighlight} from 'marked-highlight';
+import {Fragment, ReactElement} from "react";
 
 export async function generateStaticParams() {
     const articleDir = process.cwd() + '/../' + config.markdown_dir
@@ -12,10 +20,28 @@ export async function generateStaticParams() {
 }
 
 export default function Page({ params }: { params: { paths: string[] } }) {
+    marked.use(markedHighlight({
+        langPrefix: 'hljs language-',
+        highlight(code: string, lang: string) {
+            const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+            return hljs.highlight(code, { language }).value;
+        }
+    }));
+    marked.use(gfmHeadingId())
+    marked.use(mangle())
+
+    const {paths} = params
+
+    // ファイルのあるディレクトリ名
+    const dirName = paths.length > 1 ? process.cwd() + '/../' + config.markdown_dir + '/' + paths.slice(0, paths.length - 1).join('/') : process.cwd() + '/../' + config.markdown_dir
+
+    // ファイル名
+    const fileName = fs.existsSync(dirName + '/' + `${paths[paths.length-1]}.md`) ? `${paths[paths.length-1]}.md` : `${paths[paths.length-1]}.html`
+
+    const file = fs.readFileSync(dirName + '/' + fileName, 'utf-8')
+    const sourceCode = fileName.includes(".md") ? marked(file, markedConfig) : file;
+
     return (
-        <main>
-            <p>hello world</p>
-            <p>{params.paths}</p>
-        </main>
+        <main id="wrapper" dangerouslySetInnerHTML={{__html: sourceCode}}></main>
     )
 }
---
+++ app/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page({ params }: { params: { paths: string[] } }) {
+    return (
+        <main>
+            <h1 className={"text-4xl"}>hello</h1>
+        </main>
+    )
+}
