6734f0e83d7c1fba177ebf3c5c3acef4f0c8e0ad
marco <marco3jp+git@gmail.com>
2023-11-21 02:17:56 +0900

    GitHub Pagesリリースにあたってのベースディレクトリ調整

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-
 // https://astro.build/config
 export default defineConfig({
+    site: "https://marco3jp.github.io",
+    base: "/documents/",
     build: {
         format: 'file'
     },
