965c5cb3a5a770f5774d615ddc3944fda5d2c518
marco <marco3jp+git@gmail.com>
2023-11-22 02:49:56 +0900

    改行が入るようにしたりidがつくようにしたりと設定を調整

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
+import remarkBreaks from 'remark-breaks';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +12,12 @@ export default defineConfig({
         format: 'file'
     },
     integrations: [react(), tailwind()],
+    markdown: {
+        rehypePlugins: [rehypeHeadingIds],
+        remarkPlugins: [remarkBreaks],
+        shikiConfig: {
+            theme: 'dracula',
+            wrap: true
+        }
+    }
 });
